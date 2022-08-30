12f2f9a7a64a2534d4fbffb98d3a91f7b5fdbee1
akash deep <akash.shukla@janio.asia>
2022-08-30 16:55:57 +0530

    updated cv on 30 aug 2022

--- components/AccountLinks.js
+++ components/AccountLinks.js
@@ -26,7 +26,7 @@ const AccountLinks = () => {
         </a>
 
         <a
-          href="https://drive.google.com/file/d/11UCkIvBHnFdAAIsixWfZQe14TQxwcw6I/view?usp=sharing"
+          href="https://drive.google.com/file/d/15L4WGEcX2VmsdGRxYiTQ-QbLSXVXHuyh/view?usp=sharing"
           className={styles.card}
           target="_blank"
           // rel="noreferrer"
