22828b57c2672047e87d530d63a1e4fb2d5bee65
Shukla-Akash_bcgprod <shukla.akash@bcg.com>
2024-08-01 12:38:32 +0530

    Updates

---
+++ _next/static/rke0v_YUqaS97uA_ME745/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/822d8cf7a695e2a68382.css","static/chunks/pages/index-2a1b6fd04b450e43f6be.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
